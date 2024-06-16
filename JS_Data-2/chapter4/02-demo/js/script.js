09dba34f79144aec2a6e4151ebe13c3fdde9b564
yuhei <yuu11s10o@gmail.com>
2024-06-16 22:07:36 +0900

    chap4の04まで確認

--- JS_Data-2/chapter4/02-demo/js/script.js
+++ JS_Data-2/chapter4/02-demo/js/script.js
@@ -1,5 +1,20 @@
 const loading = document.querySelector('#loading');
 
-window.addEventListener('load', () => {
+//関数定義した場合
+const hidden = () => {
   loading.classList.add('loaded');
-});
+}
+window.addEventListener('load',hidden);
+
+//削除したロード画面をクリックで復活させる処理
+// const reload = () => {
+//   loading.classList.remove('loaded');
+// }
+// window.addEventListener('click',reload);
+
+
+
+//無名関数の場合
+// window.addEventListener('load', () => {
+//   loading.classList.add('loaded');
+// });
